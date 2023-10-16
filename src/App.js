0abacbecca5476ef7a98f1af295d38e9e200c9ec
sanskar <sanskargh5222@gmail.com>
2023-10-16 18:24:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Question from "./components/Question";
+import ShowResult from "./components/ShowResult";
+import { useAuth } from "./context/auth";
+
+import Error from "./components/Error";
+import { ToastContainer } from "react-toastify";
+import Admin from "./components/Admin";
+import Spinner from "./components/Spinner";
 
 function App() {
+  const [auth, setauth] = useAuth();
+  const search = useLocation().search;
+  const id = new URLSearchParams(search).get("id");
+  const exam_type = new URLSearchParams(search).get("exam-type");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer />
+
+      <Routes>
+        <Route
+          exact
+          path={auth.token || id ? "/" : "/n"}
+          element={<Login />}
+        ></Route>
+        <Route
+          exact
+          path={auth.token ? "/home" : "/n"}
+          element={<Home />}
+        ></Route>
+        {/* <Route path="/" element={<PrivateRoute />}> */}
+        {/* <Route path="/home" element={<Home />}></Route> */}
+        <Route path={`/question`} element={<Question />}></Route>
+        <Route path="/result" element={<ShowResult />}></Route>
+        <Route path="/admin" element={<Admin />}></Route>
+
+        <Route path="/*" element={<Error />} />
+        <Route path="/spinner" element={<Spinner />} />
+        {/* </Route> */}
+      </Routes>
+    </>
   );
 }
 
